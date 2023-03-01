defb15ad48c97593db36de58952dc9789037b156
听雨不是UB <1911735906@qq.com>
2023-03-01 19:06:09 +0800

    更新

--- src/main/java/top/test/dew/Main.java
+++ src/main/java/top/test/dew/Main.java
@@ -4,6 +4,12 @@ import org.bukkit.Bukkit;
 import org.bukkit.Server;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import top.test.dew.command.Fly;
+import top.test.dew.command.Ouhuang;
+import top.test.dew.command.Ubzs;
+import top.test.dew.loottables.Bossspider;
+
+import java.util.Objects;
 
 public class Main extends JavaPlugin {
 
@@ -14,6 +20,11 @@ public class Main extends JavaPlugin {
 //        Bukkit.getPluginManager().registerEvent();
         //注册监听器
         Bukkit.getPluginManager().registerEvents(new Join(), this);
+        Bukkit.getPluginManager().registerEvents(new Bossspider(), this);
+
+        Objects.requireNonNull(Bukkit.getPluginCommand("fly")).setExecutor(new Fly());
+        Objects.requireNonNull(Bukkit.getPluginCommand("ubzs")).setExecutor(new Ubzs());
+        Objects.requireNonNull(Bukkit.getPluginCommand("ouhuang")).setExecutor(new Ouhuang());
     }
 
 }
--- src/main/java/top/test/dew/loottables/Bossspider.java
+++ src/main/java/top/test/dew/loottables/Bossspider.java
@@ -1,32 +1,21 @@
 package top.test.dew.loottables;
 
+import com.google.common.eventbus.DeadEvent;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.plugin.Plugin;
 
 import java.util.Random;
 
-
-public class Bossspider implements Listener {
-    public Random random = new Random();
-
+public class Bossspider implements Listener{
     @EventHandler
-    public void kingspiderloot(EntityDeathEvent event) {
+    public static void bosspipder (EntityDeathEvent event) {
         var entity = event.getEntity();
         var player = entity.getKiller();
-        String deadentity;
-        deadentity = "pl.monster.east.Bossspider";
-        String key;
-        String Message;
-        Message = "你破开女王肚子，发现了一把镰刀";
-        if (!(entity.getName().equals(deadentity))) {
-            return;
-        }
         assert player != null;
-        var drop = event.getDrops();
-        int i = random.nextInt(100)+1;
-        if (i < 5) {
-            player.sendMessage(Message);
-        }
+        player.sendMessage("你已经击杀了怪物");
     }
 }
